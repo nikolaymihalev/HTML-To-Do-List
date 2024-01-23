a7d9dcabe166ddc020164dc59e0f3c1a0e14ac73
Nikolay Mihalev <nikimihalev1213@gmail.com>
2024-01-23 09:41:55 +0000

    Create show data function

--- script.js
+++ script.js
@@ -32,3 +32,7 @@ listContainer.addEventListener("click", function(e){
 function saveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
